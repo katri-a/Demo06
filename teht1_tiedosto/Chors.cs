3589eee2220d9be11f094668a35809bdbc02cd7a
Katri <Katri@DESKTOP>
2017-02-22 23:43:48 +0200

    teht1

---
+++ teht1_tiedosto/Chors.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht1_tiedosto
+{
+    [Serializable]
+    class Chors
+    {
+        public string Chor { get; set; }
+
+        public override string ToString()
+        {
+            return Chor;
+        }
+    }
+}
---
+++ teht1_tiedosto/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace teht1_tiedosto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chors chor = new Chors();
+
+            
+            Stream writeStream = new FileStream(@"C:\Users\Katri\Desktop\chors.txt", FileMode.Create, FileAccess.Write, FileShare.None);
+            Console.WriteLine("Give new finished chor: ");
+            IFormatter formatter = new BinaryFormatter();
+            string line = "";
+            line = Console.ReadLine();
+
+            //exit by entering empty line
+            while (!string.IsNullOrEmpty(line))
+            {
+                Console.WriteLine(string.Format("Write next or press enter to exit..."));
+                line = Console.ReadLine();
+            }
+            formatter.Serialize(writeStream, chor);
+            writeStream.Close();
+
+            Stream readStream = new FileStream(@"C:\Users\Katri\Desktop\chors.txt", FileMode.Open, FileAccess.Read, FileShare.None);
+
+            Chors readChors = (Chors)formatter.Deserialize(readStream);
+            // write proof
+            Console.WriteLine("Done chors {0}", chor);
+       
+           }
+        }
+    }
+
+
